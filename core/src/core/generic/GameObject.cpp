b9b95ec73a5772c452da76e112b4bf799d4183c0
TheTrialGamer <pascal.gutsche@gutsche-gmbh.de>
2023-03-13 08:56:38 +0100

    fixed bug in gameobject when delete; now stop on component is callled when gameobject was activated

--- core/src/core/generic/GameObject.cpp
+++ core/src/core/generic/GameObject.cpp
@@ -154,7 +154,6 @@ namespace core {
 
     void GameObject::Delete()
     {
-        this->running = false;
         this->deleted = true;
         transform.scale.x = 0.0f;
         transform.scale.y = 0.0f;
@@ -164,6 +163,8 @@ namespace core {
 		{
 			if (GetComponent<SpriteRenderer>() != components[i])
 			{
+                if (running)
+					components[i]->stop();
                 delete components[i];
                 components.erase(i + components.begin());
 			}
