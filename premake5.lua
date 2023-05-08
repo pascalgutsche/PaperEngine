253b367d6d798b30360747773c64a8f174d2f1c4
TheTrialGamer <pascal.gutsche@gutsche-gmbh.de>
2023-05-08 15:29:01 +0200

    initialized project

---
+++ premake5.lua
@@ -0,0 +1,189 @@
+workspace "Sandboxapp"
+	architecture "x64"
+	startproject "sandboxapp"
+	staticruntime "off"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "lib/core/GLFW/include"
+IncludeDir["GLAD"] = "lib/core/GLAD/include"
+IncludeDir["IMGUI"] = "lib/core/IMGUI_DOCKING"
+IncludeDir["IMPLOT"] = "lib/core/IMPLOT"
+IncludeDir["SPDLOG"] = "lib/SPDLOG/include"
+IncludeDir["MINIAUDIO"] = "lib/MINIAUDIO"
+IncludeDir["msdfgen"] = "lib/core/msdf-atlas-gen/msdfgen"
+IncludeDir["msdf_atlas_gen"] = "lib/core/msdf-atlas-gen/msdf-atlas-gen"
+
+group "dependencies"
+include "lib/core/GLFW"
+include "lib/core/GLAD"
+include "lib/core/IMGUI_DOCKING"
+include "lib/core/IMPLOT"
+include "lib/core/msdf-atlas-gen"
+group ""
+
+project "core"
+	location "core"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "_Core.h"
+	pchsource "core/src/_Core.cpp"
+
+	files
+	{
+		"%{prj.name}/lib/**.h",
+		"%{prj.name}/lib/**.gch",
+		"%{prj.name}/lib/**.hpp",
+		"%{prj.name}/lib/**.cpp",
+		"%{prj.name}/lib/**.c",
+		"%{prj.name}/lib/**.lib",
+		"%{prj.name}/lib/**.dll",
+		"%{prj.name}/lib/**.a",
+
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"lib",
+		"%{prj.name}/lib",
+		"%{prj.name}/src",
+		"%{prj.name}/src/core",
+		"%{prj.name}/src/platform",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.GLAD}",
+		"%{IncludeDir.IMGUI}",
+		"%{IncludeDir.IMPLOT}",
+		"%{IncludeDir.SPDLOG}",
+		"%{IncludeDir.MINIAUDIO}",		
+		"%{IncludeDir.msdfgen}",
+		"%{IncludeDir.msdf_atlas_gen}"
+	}
+
+	links 
+	{
+		"GLFW",
+		"GLAD",
+		"IMGUI",
+		"IMPLOT",
+		"opengl32.lib",
+		"msdf-atlas-gen"
+	}
+
+	disablewarnings 
+	{
+		"4005",
+		"4244",
+		"4312",
+		"4267",
+		"4251"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
+
+		defines
+		{
+			"CORE_PLATFORM_WINDOWS",
+			"CORE_BUILD_DLL",
+			"GLFW_INCLUDE_NONE",
+			"CORE_ENABLE_ASSERTS"
+		}
+
+	filter "configurations:Debug"
+		defines "BUILD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BUILD_RELEASE"
+		optimize "On"
+
+
+project "sandboxapp"
+	location "sandboxapp"
+	kind "ConsoleApp"
+	language "C++"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "_Game.h"
+	pchsource "sandboxapp/src/_Game.cpp"
+
+	files
+	{
+		"%{prj.name}/lib/**.h",
+		"%{prj.name}/lib/**.gch",
+		"%{prj.name}/lib/**.hpp",
+		"%{prj.name}/lib/**.cpp",
+		"%{prj.name}/lib/**.c",
+		"%{prj.name}/lib/**.lib",
+		"%{prj.name}/lib/**.dll",
+		"%{prj.name}/lib/**.a",
+
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"lib",
+		"%{prj.name}/lib",
+		"%{prj.name}/src",
+		"%{prj.name}/src/core",
+		"core/src",
+		"core/src/core",
+		"core/lib",
+		"%{IncludeDir.SPDLOG}",
+		"%{IncludeDir.MINIAUDIO}",
+		"%{IncludeDir.GLFW}",
+		"%{IncludeDir.IMGUI}",
+		"%{IncludeDir.IMPLOT}",
+		"%{IncludeDir.msdfgen}",
+		"%{IncludeDir.msdf_atlas_gen}"
+	}
+
+	links
+	{
+		"core"
+	}
+
+	disablewarnings
+	{
+		"4251",
+		"4267",
+		"4305",
+		"4244"
+	}
+	
+	filter "system:windows"
+		cppdialect "C++17"
+		systemversion "latest"
+
+		defines
+		{
+			"CORE_PLATFORM_WINDOWS",
+			"CORE_ENABLE_ASSERTS"
+		}
+
+	filter "configurations:Debug"
+		defines "BUILD_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "BUILD_RELEASE"
+		optimize "On"
---
+++ sandboxapp/assets/shaders/EdgeGeometryShader.glsl
@@ -0,0 +1,31 @@
+﻿#type vertex
+#version 330 core
+
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+// layout (location = 2) in vec2 aTexCoords;
+
+out vec4 vertexColor;
+//out vec2 vertexTexCoords;
+
+void main()
+{
+  vertexColor = vec4(aColor, 1.0);
+  gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+  //vertexTexCoords = aTexCoords;
+}
+
+#type fragment
+#version 330 core
+
+out vec4 FragColor;
+
+in vec4 vertexColor;
+// in vec2 vertexTexCoords;
+
+//uniform sampler2D endTexture[8];
+
+void main()
+{
+  FragColor = vertexColor;
+}
