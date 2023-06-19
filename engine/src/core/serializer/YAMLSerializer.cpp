3773fac534a7444ee1ad07aed9336732232d9db1
TheTrialGamer <pascal.gutsche@gutsche-gmbh.de>
2023-06-19 08:17:45 +0200

    adding serializing

--- engine/src/core/serializer/YAMLSerializer.cpp
+++ engine/src/core/serializer/YAMLSerializer.cpp
@@ -1,100 +1,6 @@
 #include "Engine.h"
 #include "YAMLSerializer.h"
 
-#include "yaml-cpp/yaml.h"
-
-#include <fstream>
-
-namespace YAML
-{
-	template <>
-	Node convert<>::encode(const glm::vec2& rhs)
-	{
-		Node node;
-		node.push_back(rhs.x);
-		node.push_back(rhs.y);
-		return node;
-	}
-
-	template <>
-	bool convert<>::decode(const Node& node, glm::vec2& rhs)
-	{
-		if (!node.IsSequence() || node.size() != 2)
-			return false;
-
-		rhs.x = node[0].as<float>();
-		rhs.y = node[1].as<float>();
-		return true;
-	}
-
-	Node encode(const glm::vec3& rhs)
-	{
-		Node node;
-		node.push_back(rhs.x);
-		node.push_back(rhs.y);
-		node.push_back(rhs.z);
-		return node;
-	}
-
-	bool decode(const Node& node, glm::vec3& rhs)
-	{
-		if (!node.IsSequence() || node.size() != 3)
-			return false;
-
-		rhs.x = node[0].as<float>();
-		rhs.y = node[1].as<float>();
-		rhs.z = node[2].as<float>();
-		return true;
-	}
-
-	template <>
-	struct convert<glm::vec4> {
-		Node encode(const glm::vec4& rhs)
-		{
-			Node node;
-			node.push_back(rhs.x);
-			node.push_back(rhs.y);
-			node.push_back(rhs.z);
-			node.push_back(rhs.w);
-			return node;
-		}
-
-		bool decode(const Node& node, glm::vec4& rhs)
-		{
-			if (!node.IsSequence() || node.size() != 4)
-				return false;
-
-			rhs.x = node[0].as<float>();
-			rhs.y = node[1].as<float>();
-			rhs.z = node[2].as<float>();
-			rhs.w = node[3].as<float>();
-			return true;
-		}
-	};
-
-	template <>
-	struct convert<Vertex> {
-		Node encode(const Vertex& rhs)
-		{
-			Node node;
-			node.push_back(rhs.position);
-			node.push_back(rhs.normal);
-			node.push_back(rhs.color);
-			return node;
-		}
-
-		bool decode(const Node& node, Vertex& rhs)
-		{
-			if (!node.IsSequence() || node.size() != 3)
-				return false;
-
-			rhs.position = node[0].as<glm::vec3>();
-			rhs.normal = node[1].as<glm::vec3>();
-			rhs.color = node[2].as<glm::vec4>();
-			return true;
-		}
-	};
-}
 
 namespace engine
 {
@@ -112,11 +18,10 @@ namespace engine
 		out << YAML::BeginMap;
 		out << YAML::Key << "Entities" << YAML::Value << YAML::BeginSeq;
 		out << YAML::Key << "TextComponent";
-		out << YAML::BeginMap; // TextComponent
+		out << YAML::BeginMap << "TextComponent"; // TextComponent
 
 		out << YAML::Key << "TextString" << YAML::Value << "textComponent.TextString";
-		// TODO: textComponent.FontAsset
-		out << YAML::Key << "Color" << YAML::Value << "textComponent.Color" << 3495;
+		out << YAML::Key << "Color" << YAML::Value << "textComponent.Color";
 		out << YAML::Key << "Kerning" << YAML::Value << 795348;
 		out << YAML::Key << "LineSpacing" << YAML::Value << 59435.957;
 
--- engine/src/core/serializer/YAMLSerializer.h
+++ engine/src/core/serializer/YAMLSerializer.h
@@ -102,19 +102,19 @@ namespace YAML
 
 namespace engine
 {
-	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec2& v)
+	inline YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec2& v)
 	{
 		out << YAML::BeginSeq << v.x << v.y << YAML::EndSeq;
 		return out;
 	}
 
-	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& v)
+	inline YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec3& v)
 	{
 		out << YAML::BeginSeq << v.x << v.y << v.z << YAML::EndSeq;
 		return out;
 	}
 
-	YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& v)
+	inline YAML::Emitter& operator<<(YAML::Emitter& out, const glm::vec4& v)
 	{
 		out << YAML::BeginSeq << v.x << v.y << v.z << v.w << YAML::EndSeq;
 		return out;
