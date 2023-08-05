4078dbc7a661bf28166645c6f8e06b88236f41a7
TheTrialGamer <pascal.gutsche@gutsche-gmbh.de>
2023-08-05 23:46:49 +0200

    added buffer class

---
+++ engine/src/core/generic/Buffer.cpp
@@ -0,0 +1,73 @@
+﻿#include "Engine.h"
+#include "Buffer.h"
+
+namespace Paper
+{
+	Buffer::Buffer()
+		: data(nullptr), size(0)
+	{
+	}
+
+	Buffer::Buffer(uint32_t size)
+	{
+		Allocate(size);
+	}
+
+	Buffer::Buffer(void* data, uint32_t size)
+		: data(data), size(size)
+	{
+	}
+
+	Buffer::~Buffer()
+	{
+		Release();
+	}
+
+	Buffer Buffer::Copy() const
+	{
+		Buffer buffer(size);
+		memcpy(buffer.data, data, size);
+		return buffer;
+	}
+
+	void Buffer::CopyTo(Buffer& dst) const
+	{
+		dst.Allocate(size);
+		memcpy(dst.data, data, size);
+	}
+
+	void Buffer::CopyFrom(const Buffer& src)
+	{
+		Allocate(src.size);
+		memcpy(data, src.data, src.size);
+	}
+
+	void Buffer::Set(void* data, uint32_t size)
+	{
+		Release();
+		this->data = data;
+		this->size = size;
+	}
+
+	void Buffer::Allocate(uint32_t size)
+	{
+		Release();
+		if (!size)
+			return;
+
+		data = new byte[size];
+		this->size = size;
+	}
+
+	void Buffer::Release()
+	{
+		delete[](byte*)data;
+		size = 0;
+		data = nullptr;
+	}
+
+	void Buffer::Nullify() const
+	{
+		memset(data, 0, size);
+	}
+}
---
+++ engine/src/core/generic/Buffer.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+
+namespace Paper
+{
+	class Buffer
+	{
+		using byte = uint8_t;
+	public:
+		void* data;
+		uint32_t size;
+
+		Buffer();
+		Buffer(uint32_t size);
+		Buffer(void* data, uint32_t size);
+
+		~Buffer();
+
+		Buffer Copy() const;
+		void CopyTo(Buffer& dst) const;
+		void CopyFrom(const Buffer& src);
+
+		void Set(void* data, uint32_t size);
+
+		void Allocate(uint32_t size);
+		void Release();
+
+		void Nullify() const;
+
+		template<typename T>
+		T& Read()
+		{
+			return *(T*)data;
+		}
+
+		template<typename T>
+		T* As()
+		{
+			return (T*)data;
+		}
+	};
+}
+
