735d11739362f3d06ce36694cc0fd598dcf8d1b5
TheTrialGamer <pascal.gutsche@gutsche-gmbh.de>
2023-09-24 23:56:03 +0200

    added WTController.cs to index

--- PaperEditor/Sandbunker/assets/scripts/Source/WTBird/WTController.cs
+++ PaperEditor/Sandbunker/assets/scripts/Source/WTBird/WTController.cs
@@ -1,7 +1,45 @@
-﻿namespace Sandbunker.assets.scripts.Source.WTBird
+﻿using System.Runtime;
+using Paper;
+
+namespace Sandbunker
 {
-    public class WTController
+    public class WTController : Entity
     {
-        
+        public float speed = 5.0f;
+
+        private TransformComponent transformComponent;
+
+        public override void OnCreate()
+        {
+            transformComponent = GetComponent<TransformComponent>();
+        }
+
+        public override void OnUpdate(float dt)
+        {
+            Vec3 pos = transformComponent.Position;
+
+            if (Input.IsKeyDown(Key.W))
+            {
+                pos.Z -= speed * dt;
+            }
+            if (Input.IsKeyDown(Key.A))
+            {
+                pos.X -= speed * dt;
+            }
+            if (Input.IsKeyDown(Key.S))
+            {
+                pos.Z += speed * dt;
+            }
+            if (Input.IsKeyDown(Key.D))
+            {
+                pos.X += speed * dt;
+            }
+            if (Input.IsKeyDown(Key.E))
+                pos.Y += speed * dt;
+            if (Input.IsKeyDown(Key.Q))
+                pos.Y -= speed * dt;
+
+            transformComponent.Position = pos;
+        }
     }
 }
